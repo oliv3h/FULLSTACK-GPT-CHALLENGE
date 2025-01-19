80be7a49de783db89ea9c3dd9615e654a9761572
HongEuiYoung <49014188+oliv3h@users.noreply.github.com>
2025-01-20 01:49:53 +0900

    Update 02_QUIZ.py

--- pages/02_QUIZ.py
+++ pages/02_QUIZ.py
@@ -83,275 +83,275 @@ with st.sidebar:
         if topic:
             docs = wiki_search(topic)
 
-
-function = {
-    "name": "create_quiz",
-    "description": "function that takes a list of questions and answers and returns a quiz",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "questions": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "question": {
-                            "type": "string",
-                        },
-                        "answers": {
-                            "type": "array",
-                            "items": {
-                                "type": "object",
-                                "properties": {
-                                    "answer": {
-                                        "type": "string",
+if key:
+    function = {
+        "name": "create_quiz",
+        "description": "function that takes a list of questions and answers and returns a quiz",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "questions": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "question": {
+                                "type": "string",
+                            },
+                            "answers": {
+                                "type": "array",
+                                "items": {
+                                    "type": "object",
+                                    "properties": {
+                                        "answer": {
+                                            "type": "string",
+                                        },
+                                        "correct": {
+                                            "type": "boolean",
+                                        },
                                     },
-                                    "correct": {
-                                        "type": "boolean",
-                                    },
+                                    "required": ["answer", "correct"],
                                 },
-                                "required": ["answer", "correct"],
                             },
                         },
+                        "required": ["question", "answers"],
                     },
-                    "required": ["question", "answers"],
-                },
-            }
+                }
+            },
+            "required": ["questions"],
         },
-        "required": ["questions"],
-    },
-}
+    }
 
-llm = ChatOpenAI(
-    temperature=0.1,
-    model="gpt-4o-mini",
-    streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()],
-    api_key=key
-).bind(
-    function_call={
-        "name": "create_quiz"
-    },
-    functions=[function]
-)
+    llm = ChatOpenAI(
+        temperature=0.1,
+        model="gpt-4o-mini",
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        api_key=key
+    ).bind(
+        function_call={
+            "name": "create_quiz"
+        },
+        functions=[function]
+    )
 
 
-questions_prompt = ChatPromptTemplate.from_messages(
+    questions_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", 
+                """
+    You are a helpful assistant that is role playing as a teacher.
+
+    Based ONLY on the following context make 10 questions to test the user's
+    knowledge about the text.
+
+    Each question should have 4 answers, three of them must be incorrect and 
+    one should be correct.
+
+    Use (o) to signal the correct answer.
+
+    Question examples:
+
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+
+    Question: What is the capital of Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+
+    Question: When was Avater released?
+    Answers: 2007|2001|2009(o)|1998
+
+    Question: Who was Julius Caeser?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+
+    Your turn!
+
+    Context: {context}
+                """)
+            ]
+        )
+
+    def format_docs(docs):
+        return "\n\n".join(document.page_content for document in docs)
+
+    question_chain = {"context": format_docs} | questions_prompt | llm
+
+    questions_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", 
-             """
-You are a helpful assistant that is role playing as a teacher.
-
-Based ONLY on the following context make 10 questions to test the user's
-knowledge about the text.
-
-Each question should have 4 answers, three of them must be incorrect and 
-one should be correct.
-
-Use (o) to signal the correct answer.
-
-Question examples:
-
-Question: What is the color of the ocean?
-Answers: Red|Yellow|Green|Blue(o)
-
-Question: What is the capital of Georgia?
-Answers: Baku|Tbilisi(o)|Manila|Beirut
-
-Question: When was Avater released?
-Answers: 2007|2001|2009(o)|1998
-
-Question: Who was Julius Caeser?
-Answers: A Roman Emperor(o)|Painter|Actor|Model
-
-Your turn!
-
-Context: {context}
-             """)
+            (
+                "system",
+                """
+        You are a helpful assistant that is role playing as a teacher.
+            
+        Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
+        
+        Each question should have 4 answers, three of them must be incorrect and one should be correct.
+            
+        Use (o) to signal the correct answer.
+            
+        Question examples:
+            
+        Question: What is the color of the ocean?
+        Answers: Red|Yellow|Green|Blue(o)
+            
+        Question: What is the capital or Georgia?
+        Answers: Baku|Tbilisi(o)|Manila|Beirut
+            
+        Question: When was Avatar released?
+        Answers: 2007|2001|2009(o)|1998
+            
+        Question: Who was Julius Caesar?
+        Answers: A Roman Emperor(o)|Painter|Actor|Model
+            
+        Your turn!
+            
+        Context: {context}
+    """,
+            )
+        ]
+    )
+    questions_chain = {"context": format_docs} | questions_prompt | llm
+    formatting_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+        You are a powerful formatting 
+        You format exam questions into JSON format.
+        Answers with (o) are the correct ones.
+        
+        Example Input:
+        Question: What is the color of the ocean?
+        Answers: Red|Yellow|Green|Blue(o)
+            
+        Question: What is the capital or Georgia?
+        Answers: Baku|Tbilisi(o)|Manila|Beirut
+            
+        Question: When was Avatar released?
+        Answers: 2007|2001|2009(o)|1998
+            
+        Question: Who was Julius Caesar?
+        Answers: A Roman Emperor(o)|Painter|Actor|Model
+        
+        
+        Example Output:
+        
+        ```json
+        {{ "questions": [
+                {{
+                    "question": "What is the color of the ocean?",
+                    "answers": [
+                            {{
+                                "answer": "Red",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Yellow",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Green",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Blue",
+                                "correct": true
+                            }},
+                    ]
+                }},
+                            {{
+                    "question": "What is the capital or Georgia?",
+                    "answers": [
+                            {{
+                                "answer": "Baku",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Tbilisi",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "Manila",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Beirut",
+                                "correct": false
+                            }},
+                    ]
+                }},
+                            {{
+                    "question": "When was Avatar released?",
+                    "answers": [
+                            {{
+                                "answer": "2007",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "2001",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "2009",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "1998",
+                                "correct": false
+                            }},
+                    ]
+                }},
+                {{
+                    "question": "Who was Julius Caesar?",
+                    "answers": [
+                            {{
+                                "answer": "A Roman Emperor",
+                                "correct": true
+                            }},
+                            {{
+                                "answer": "Painter",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Actor",
+                                "correct": false
+                            }},
+                            {{
+                                "answer": "Model",
+                                "correct": false
+                            }},
+                    ]
+                }}
+            ]
+        }}
+        ```
+        Your turn!
+        Questions: {context}
+    """,
+            )
         ]
     )
 
-def format_docs(docs):
-    return "\n\n".join(document.page_content for document in docs)
+    formatting_chain = formatting_prompt | llm
 
-question_chain = {"context": format_docs} | questions_prompt | llm
+    use_function_calling_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+        You are a helpful assistant that is role playing as a teacher.
+            
+        Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
 
-questions_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-    You are a helpful assistant that is role playing as a teacher.
-         
-    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
-    
-    Each question should have 4 answers, three of them must be incorrect and one should be correct.
-         
-    Use (o) to signal the correct answer.
-         
-    Question examples:
-         
-    Question: What is the color of the ocean?
-    Answers: Red|Yellow|Green|Blue(o)
-         
-    Question: What is the capital or Georgia?
-    Answers: Baku|Tbilisi(o)|Manila|Beirut
-         
-    Question: When was Avatar released?
-    Answers: 2007|2001|2009(o)|1998
-         
-    Question: Who was Julius Caesar?
-    Answers: A Roman Emperor(o)|Painter|Actor|Model
-         
-    Your turn!
-         
-    Context: {context}
-""",
-        )
-    ]
-)
-questions_chain = {"context": format_docs} | questions_prompt | llm
-formatting_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-    You are a powerful formatting 
-    You format exam questions into JSON format.
-    Answers with (o) are the correct ones.
-     
-    Example Input:
-    Question: What is the color of the ocean?
-    Answers: Red|Yellow|Green|Blue(o)
-         
-    Question: What is the capital or Georgia?
-    Answers: Baku|Tbilisi(o)|Manila|Beirut
-         
-    Question: When was Avatar released?
-    Answers: 2007|2001|2009(o)|1998
-         
-    Question: Who was Julius Caesar?
-    Answers: A Roman Emperor(o)|Painter|Actor|Model
-    
-     
-    Example Output:
-     
-    ```json
-    {{ "questions": [
-            {{
-                "question": "What is the color of the ocean?",
-                "answers": [
-                        {{
-                            "answer": "Red",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Yellow",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Green",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Blue",
-                            "correct": true
-                        }},
-                ]
-            }},
-                        {{
-                "question": "What is the capital or Georgia?",
-                "answers": [
-                        {{
-                            "answer": "Baku",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Tbilisi",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "Manila",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Beirut",
-                            "correct": false
-                        }},
-                ]
-            }},
-                        {{
-                "question": "When was Avatar released?",
-                "answers": [
-                        {{
-                            "answer": "2007",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2001",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "2009",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "1998",
-                            "correct": false
-                        }},
-                ]
-            }},
-            {{
-                "question": "Who was Julius Caesar?",
-                "answers": [
-                        {{
-                            "answer": "A Roman Emperor",
-                            "correct": true
-                        }},
-                        {{
-                            "answer": "Painter",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Actor",
-                            "correct": false
-                        }},
-                        {{
-                            "answer": "Model",
-                            "correct": false
-                        }},
-                ]
-            }}
+        The questions has Level of Difficult. This Questions has {level} level.
+        
+        Each question should have 4 answers, three of them must be incorrect and one should be correct.
+
+        Context: {context}
+    """,
+            )
         ]
-     }}
-    ```
-    Your turn!
-    Questions: {context}
-""",
-        )
-    ]
-)
-
-formatting_chain = formatting_prompt | llm
-
-use_function_calling_prompt = ChatPromptTemplate.from_messages(
-    [
-        (
-            "system",
-            """
-    You are a helpful assistant that is role playing as a teacher.
-         
-    Based ONLY on the following context make 10 questions to test the user's knowledge about the text.
-
-    The questions has Level of Difficult. This Questions has {level} level.
-    
-    Each question should have 4 answers, three of them must be incorrect and one should be correct.
-
-    Context: {context}
-""",
-        )
-    ]
-)
+    )
 
 
 if not docs:
